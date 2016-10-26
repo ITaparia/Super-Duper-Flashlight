32dd89f31fba24f6ef2903ef676ba314928851ab
Ishan Taparia <dualstrike99@gmail.com>
2016-10-26 18:42:41 -0400

    Initial Commit

---
+++ Super Duper Flashlight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Duper Flashlight
+//
+//  Created by Ishan Taparia on 10/26/16.
+//  Copyright © 2016 Ishan Taparia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
